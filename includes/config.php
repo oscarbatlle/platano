a4bc8c6492eb75a4618f1b370d9f07f34b7f184f
oscarbatlle <oscarbatlle@gmail.com>
2015-02-10 23:16:44 -0500

    Add config.php autoload function

--- includes/config.php
+++ includes/config.php
@@ -24,3 +24,28 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 # Set Timezone
 
 date_default_timezone_set('America/New_York');
+
+function __autoload($class)
+{
+    $class = strtolower($class);
+    # Call from within assets adjust the path
+    $classpath = 'classes/class.' . $class . '.php';
+    if (file_exists($classpath))
+    {
+        require_once $classpath;
+    }
+
+    # Call from within admin adjust the path
+    $classpath = '../classes/class.' . $class . '.php';
+    if (file_exists($classpath))
+    {
+        require_once $classpath;
+    }
+
+    # Call from within admin adjust the path
+    $classpath = '../../classes/class.' . $class . '.php';
+    if (file_exists($classpath))
+    {
+        require_once $classpath;
+    }
+}
