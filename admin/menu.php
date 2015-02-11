6749ef4106a2d7999b878606cb4e5bf64e9b3d2e
oscarbatlle <oscarbatlle@gmail.com>
2015-02-11 11:50:51 -0500

    Update menu structure

--- admin/menu.php
+++ admin/menu.php
@@ -1,8 +1,8 @@
-<h1>Blog</h1>
+<h1>Pl&aacute;tano - The Super-lightweight blog engine</h1>
 <ul id='adminmenu'>
-    <li><a href='index.php'>Blog</a></li>
+    <li><a href='index.php'>Posts</a></li>
     <li><a href='users.php'>Users</a></li>
-    <li><a href="../" target="_blank">View Website</a></li>
+    <li><a href="../" target="_blank">Blog</a></li>
     <li><a href='logout.php'>Logout</a></li>
 </ul>
 <div class='clear'></div>
