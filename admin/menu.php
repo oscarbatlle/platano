edf43323519d68357dbbedc0d972830bb12b67c0
Oscar Batlle <oscarbatlle@gmail.com>
2015-03-03 09:09:29 -0500

    Modify admin/menu.php
    Add HTML5 Semantic Elements to describes its meaning to both, the browser and the developer.

--- admin/menu.php
+++ admin/menu.php
@@ -1,9 +1,10 @@
-<h1>Pl&aacute;tano - The Super-lightweight blog engine</h1>
-<ul id='adminmenu'>
-    <li><a href='index.php'>Posts</a></li>
-    <li><a href='users.php'>Users</a></li>
-    <li><a href="../" target="_blank">Blog</a></li>
-    <li><a href='logout.php'>Logout</a></li>
-</ul>
+<nav>
+    <ul id='adminmenu'>
+        <li><a href='index.php'>Posts</a></li>
+        <li><a href='users.php'>Users</a></li>
+        <li><a href="../" target="_blank">Blog</a></li>
+        <li><a href='logout.php'>Logout</a></li>
+    </ul>
+</nav>
 <div class='clear'></div>
 <hr/>
