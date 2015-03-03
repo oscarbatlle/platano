8a3760b17f11a36ba3f3490f434f485b0309d280
Oscar Batlle <oscarbatlle@gmail.com>
2015-03-03 12:11:13 -0500

    Modify css/styles.css
    add adminmenu class to the ul list

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,5 @@
 <nav>
-    <ul id='adminmenu'>
+    <ul class='adminmenu'>
         <li><a href='index.php'>Posts</a></li>
         <li><a href='users.php'>Users</a></li>
         <li><a href="../" target="_blank">Blog</a></li>
