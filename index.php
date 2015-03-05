3ca286b30284b52087c6e8bb7ccf1dd1f580a519
Oscar Batlle <oscarbatlle@gmail.com>
2015-03-04 23:12:57 -0500

    Modify index.php
    include sidebar.php

--- index.php
+++ index.php
@@ -63,16 +63,7 @@ require('includes/config.php');
             ?>
         </main>
 
-        <aside class="sidebar">
-            <h2>WHO AM I ?</h2>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-        </aside>
+        <?php include('sidebar.php') ?>
 
     </div>
     <footer>
