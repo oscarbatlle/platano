a01bfc4d3bedb1898046f98e5b88486bc3fa2237
Oscar Batlle <oscarbatlle@gmail.com>
2015-03-05 00:14:32 -0500

    Modify viewpost.php
    Add meta tag viewport for mobile devices

--- viewpost.php
+++ viewpost.php
@@ -21,6 +21,7 @@ if ($row['postID'] == '')
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $row['postTitle']; ?></title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
